b2e9fd00cdf329676961ddb306496788acf0f0eb
Andrea Allais <andrea@getcruise.com>
2018-03-21 08:29:09 -0700

    Added generic run.py

--- analysis/run.py
+++ analysis/run.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
+from __future__ import division
 import subprocess
+import os
 import numpy as np
 import signal
 import uuid
-
+import random
+import argparse
+from collections import namedtuple
 current_process = None
 
 def signal_handler(signum, frame):
@@ -13,46 +17,68 @@ def signal_handler(signum, frame):
         current_process.send_signal(signum)
 
 
-def format_options(shape, prob, seed, measure_every, n_measure, tag):
-    options = ['--shape'] + map(str, shape) + [ 
+def format_options(shape, prob, seed, measure_every,
+                   n_measure, tag, output_path):
+    options = ['--shape'] + map(str, shape) + [
                '--prob', "{:.8f}".format(prob),
                '--seed', str(seed),
-               '--measure-every', str(measure_every), 
+               '--measure-every', str(measure_every),
                '--n-measure', str(n_measure),
                '--tag', str(tag)]
-    file_name = uuid.uuid1().hex + '.bin'
+    file_name = os.path.join(output_path, uuid.uuid1().hex + '.bin')
     options += ['--output', file_name]
     return options
-    
-def main():
+
+Parameters = namedtuple('Parameters',
+                        ['prob', 'L', 'measure_every', 'n_measure'])
+
+def read_tasks(task_file):
+    tasks = list()
+    with open(task_file) as in_file:
+        for row in in_file:
+            if row.startswith('#'):
+                continue
+            tokens = row.split(',')
+            if len(tokens) != 4:
+                continue
+            tokens = [token.lstrip(' ').rstrip('\n') for token in tokens]
+            tasks.append(Parameters(
+                prob=(int(tokens[0], 0) / (2**32)),
+                L=int(tokens[1]),
+                measure_every=(1 << int(tokens[2])),
+                n_measure=(1 << int(tokens[3]))))
+    return tasks
+
+
+def main(task_file, output_path, n_dims, executable):
     global current_process
+    tasks = read_tasks(task_file)
     signal.signal(signal.SIGINT, signal_handler)
-    executable = '../bin/run'
-    prob = 0.645
-    for L, measure_every, n_measure in [
-        #(8,            64,  16 * 1024),
-        #(12,           64,  16 * 1024),
-        #(16,          128,  16 * 1024),
-        #(24,          128,  16 * 1024),
-        #(32,          256, 1024 * 1024),
-        #(48,          256,  1024 * 1024),
-        #(64,          512,  1024 * 1024),
-        (128,    2 * 1024,  1024 * 1024),
-        #(256,    8 * 1024,   32 * 1024),
-        #(512,   32 * 1024,    8 * 1024),
-        #(1024, 512 * 1024,   4 * 1024),
-        ]:
+    for parameters in tasks:
         options = format_options(
-            shape=[L, L, L],
-            prob=prob,
-            seed=6,
-            measure_every=measure_every,
-            n_measure=n_measure,
-            tag='autocorrelation-1')
+            shape=([parameters.L] * n_dims),
+            prob=parameters.prob,
+            seed=random.randint(0, 2**32 - 1),
+            measure_every=parameters.measure_every,
+            n_measure=parameters.n_measure,
+            tag='',
+            output_path=output_path)
         command = [executable] + options
         print(' '.join(command))
         current_process = subprocess.Popen(command)
         current_process.wait()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Run simulations.')
+    parser.add_argument(
+        'task_file', type=str, help='File describing tasks')
+    parser.add_argument(
+        'output_path', type=str, help='Path where to place output')
+    parser.add_argument(
+        '--n-dims', type=int, default=4, help='number of dimensions')
+    parser.add_argument(
+        '--executable', type=str,
+        default='/home/andrea/Dropbox/Documents/rg_ising/bin/run',
+        help='Path to executable')
+    args = parser.parse_args()
+    main(**vars(args))
