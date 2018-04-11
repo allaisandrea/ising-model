6009c0c81d6dca3511d610f600ed6f97a435fcc7
Andrea Allais <andrea@getcruise.com>
2018-04-11 06:16:47 -0700

    YAML task file, stop all on interrupt

--- analysis/run.py
+++ analysis/run.py
@@ -8,19 +8,24 @@ import signal
 import uuid
 import random
 import argparse
+import yaml
 from collections import namedtuple
-current_process = None
 
-def signal_handler(signum, frame):
-    global current_process
+
+current_process = None
+keep_running = True
+
+def sigint_handler(signum, frame):
+    global current_process, keep_running
+    keep_running = False
     if current_process is not None:
         current_process.send_signal(signum)
 
 
-def format_options(shape, prob, seed, measure_every,
+def format_options(shape, i_prob, seed, measure_every,
                    n_measure, tag, output_path):
     options = ['--shape'] + map(str, shape) + [
-               '--prob', "{:.8f}".format(prob),
+               '--i-prob', i_prob,
                '--seed', str(seed),
                '--measure-every', str(measure_every),
                '--n-measure', str(n_measure),
@@ -30,34 +35,31 @@ def format_options(shape, prob, seed, measure_every,
     return options
 
 Parameters = namedtuple('Parameters',
-                        ['prob', 'L', 'measure_every', 'n_measure'])
+                        ['i_prob', 'L', 'measure_every', 'n_measure'])
 
 def read_tasks(task_file):
-    tasks = list()
     with open(task_file) as in_file:
-        for row in in_file:
-            if row.startswith('#'):
-                continue
-            tokens = row.split(',')
-            if len(tokens) != 4:
-                continue
-            tokens = [token.lstrip(' ').rstrip('\n') for token in tokens]
-            tasks.append(Parameters(
-                prob=(int(tokens[0], 0) / (2**32)),
-                L=int(tokens[1]),
-                measure_every=(1 << int(tokens[2])),
-                n_measure=(1 << int(tokens[3]))))
+        tasks = yaml.load(in_file)
+    tasks = [
+        Parameters(
+            i_prob=tokens[0],
+            L=int(tokens[1]),
+            measure_every=(1 << int(tokens[2])),
+            n_measure=(1 << int(tokens[3])))
+        for tokens in tasks]
     return tasks
 
 
 def main(task_file, output_path, n_dims, executable):
-    global current_process
+    global current_process, keep_running
     tasks = read_tasks(task_file)
-    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGINT, sigint_handler)
     for parameters in tasks:
+        if not keep_running:
+            break
         options = format_options(
             shape=([parameters.L] * n_dims),
-            prob=parameters.prob,
+            i_prob=parameters.i_prob,
             seed=random.randint(0, 2**32 - 1),
             measure_every=parameters.measure_every,
             n_measure=parameters.n_measure,
@@ -71,7 +73,7 @@ def main(task_file, output_path, n_dims, executable):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run simulations.')
     parser.add_argument(
-        'task_file', type=str, help='File describing tasks')
+        'task_file', type=str, help='YAML file describing tasks')
     parser.add_argument(
         'output_path', type=str, help='Path where to place output')
     parser.add_argument(
