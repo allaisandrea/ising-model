29a60e6f88dd2bfba40e3721dea1f5b4e46a5399
andrea <allais.andrea@gmail.com>
2019-07-09 06:49:45 -0700

    Improved progress

--- src/progress.h
+++ src/progress.h
@@ -1,29 +1,27 @@
 #pragma once
 #include <ctime>
+#include <sstream>
 #include <string>
 
-struct ProgressString {
-    ProgressString(const std::time_t start_time, const std::time_t now,
-                   uint64_t progress, uint64_t max_progress)
-        : start_time(start_time), now(now), progress(progress),
-          max_progress(max_progress) {}
-    std::time_t start_time;
-    std::time_t now;
-    uint64_t progress;
-    uint64_t max_progress;
-};
+class ProgressIndicator {
+    std::time_t _start_time;
+    uint64_t _max_progress;
 
-std::ostream &operator<<(std::ostream &strm, const ProgressString &ps) {
-    const auto flags = strm.flags();
-    strm << std::fixed << std::setprecision(2) << std::setw(6)
-         << 100.0 * ps.progress / ps.max_progress << "%";
-    if (ps.progress > 0) {
-        const double elapsed = std::difftime(ps.now, ps.start_time);
-        const std::time_t eta =
-            ps.start_time + ps.max_progress * elapsed / ps.progress;
-        const char *time_string = std::asctime(std::localtime(&eta));
-        strm << " ETA " << std::string(time_string, time_string + 24);
+  public:
+    ProgressIndicator(const std::time_t start_time, uint64_t max_progress)
+        : _start_time(start_time), _max_progress(max_progress) {}
+
+    std::string string(const std::time_t now, uint64_t progress) {
+        std::ostringstream strm;
+        strm << std::fixed << std::setprecision(2) << std::setw(6)
+             << 100.0 * progress / _max_progress << "%";
+        if (progress > 0) {
+            const double elapsed = std::difftime(now, _start_time);
+            const std::time_t eta =
+                _start_time + _max_progress * elapsed / progress;
+            const char *time_string = std::asctime(std::localtime(&eta));
+            strm << " ETA " << std::string(time_string, time_string + 24);
+        }
+        return strm.str();
     }
-    strm.flags(flags);
-    return strm;
-}
+};
--- src/test.cpp
+++ src/test.cpp
@@ -879,23 +879,25 @@ TEST(Timer, Timer) {
 }
 
 TEST(Progress, ProgressString) {
-    std::ostringstream strm;
-    strm << ProgressString(0, 60, 0, 100);
-    EXPECT_EQ(strm.str(), "  0.00%");
-
-    strm.str("");
-    strm << ProgressString(0, 60, 100, 100);
-    EXPECT_EQ(strm.str(), "100.00% ETA Wed Dec 31 16:01:00 1969");
-
-    strm.str("");
-    strm << ProgressString(0, 60, 50, 100);
-    EXPECT_EQ(strm.str(), " 50.00% ETA Wed Dec 31 16:02:00 1969");
-
-    strm.str("");
-    strm << ProgressString(30, 90, 50, 100);
-    EXPECT_EQ(strm.str(), " 50.00% ETA Wed Dec 31 16:02:30 1969");
-
-    strm.str("");
-    strm << ProgressString(0, 3600, 1, 1ul << 32);
-    EXPECT_EQ(strm.str(), "  0.00% ETA Sun Jul 18 09:00:00     ");
+    {
+        ProgressIndicator progress(0, 100);
+        EXPECT_EQ(progress.string(60, 0), "  0.00%");
+        EXPECT_EQ(progress.string(60, 50),
+                  " 50.00% ETA Wed Dec 31 16:02:00 1969");
+        EXPECT_EQ(progress.string(60, 100),
+                  "100.00% ETA Wed Dec 31 16:01:00 1969");
+    }
+    {
+        ProgressIndicator progress(30, 100);
+        EXPECT_EQ(progress.string(60, 0), "  0.00%");
+        EXPECT_EQ(progress.string(90, 50),
+                  " 50.00% ETA Wed Dec 31 16:02:30 1969");
+        EXPECT_EQ(progress.string(90, 100),
+                  "100.00% ETA Wed Dec 31 16:01:30 1969");
+    }
+    {
+        ProgressIndicator progress(0, 1ul << 32);
+        EXPECT_EQ(progress.string(3600, 1),
+                  "  0.00% ETA Sun Jul 18 09:00:00     ");
+    }
 }
