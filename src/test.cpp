00f31fece4b8d4356d879e7db012ecf2ed85a9d8
andrea <allais.andrea@gmail.com>
2019-07-10 07:00:43 -0700

    Added udh_arguments

--- src/test.cpp
+++ src/test.cpp
@@ -15,6 +15,7 @@
 #include "tensor.h"
 #include "throttle.h"
 #include "timer.h"
+#include "udh_arguments.h"
 #include "udh_io.h"
 #include "udh_measure.h"
 #include "udh_metropolis_algorithm.h"
@@ -917,4 +918,65 @@ TEST(Throttle, Throttle) {
     MockClock::time = 36;
     throttle([&counter]() { ++counter; });
     EXPECT_EQ(counter, 2ul);
+}
+
+TEST(UdhArguments, UdhArguments) {
+    udh::Parameters parameters;
+    const char *args1[] = {"run-simulation", "--help"};
+    EXPECT_FALSE(ParseArgs(2, args1, &parameters));
+
+    // clang-format off
+    const char *args2[] = { "run-simulation",
+        "--J", "0.5",
+        "--mu", "0.25",
+        "--shape", "3", "4",
+        "--n-wolff", "5",
+        "--n-metropolis", "6",
+        "--measure-every", "100",
+        "--n-measure", "200"};
+    // clang-format on
+
+    EXPECT_TRUE(ParseArgs(16, args2, &parameters));
+
+    EXPECT_EQ(parameters.j(), 0.5);
+    EXPECT_EQ(parameters.mu(), 0.25);
+    EXPECT_EQ(parameters.shape().size(), 2);
+    EXPECT_EQ(parameters.shape(0), 3ul);
+    EXPECT_EQ(parameters.shape(1), 4ul);
+    EXPECT_EQ(parameters.n_wolff(), 5ul);
+    EXPECT_EQ(parameters.n_metropolis(), 6ul);
+    EXPECT_EQ(parameters.measure_every(), 100ul);
+    EXPECT_EQ(parameters.n_measure(), 200ul);
+    EXPECT_GT(parameters.seed(), 0ul);
+    EXPECT_NE(parameters.id(), "");
+    EXPECT_EQ(parameters.tag(), "");
+
+    // clang-format off
+    const char *args3[] = { "run-simulation",
+        "--J", "0.5",
+        "--mu", "0.25",
+        "--shape", "3", "4",
+        "--n-wolff", "5",
+        "--n-metropolis", "6",
+        "--measure-every", "100",
+        "--n-measure", "200",
+        "--seed", "7",
+        "--id", "5ab4fd9aa",
+        "--tag", "foo"};
+    // clang-format on
+
+    EXPECT_TRUE(ParseArgs(22, args3, &parameters));
+
+    EXPECT_EQ(parameters.j(), 0.5);
+    EXPECT_EQ(parameters.mu(), 0.25);
+    EXPECT_EQ(parameters.shape().size(), 2);
+    EXPECT_EQ(parameters.shape(0), 3ul);
+    EXPECT_EQ(parameters.shape(1), 4ul);
+    EXPECT_EQ(parameters.n_wolff(), 5ul);
+    EXPECT_EQ(parameters.n_metropolis(), 6ul);
+    EXPECT_EQ(parameters.measure_every(), 100ul);
+    EXPECT_EQ(parameters.n_measure(), 200ul);
+    EXPECT_EQ(parameters.seed(), 7ul);
+    EXPECT_EQ(parameters.id(), "5ab4fd9aa");
+    EXPECT_EQ(parameters.tag(), "foo");
 }
---
+++ src/udh_arguments.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "udh_simulation.pb.h"
+#include <boost/program_options.hpp>
+#include <random>
+
+bool ParseArgs(int argc, const char *argv[], udh::Parameters *parameters) {
+    using namespace boost::program_options;
+
+    std::vector<uint32_t> shape;
+    double J, mu;
+    uint32_t n_wolff, n_metropolis, measure_every, n_measure, seed;
+    std::string id, tag;
+    seed = std::random_device()();
+    std::mt19937 rng(seed);
+    std::ostringstream strm;
+    strm << std::hex << rng() << rng();
+    id = strm.str();
+
+    options_description description{"Options"};
+    auto &&add_option = description.add_options();
+    add_option("help,h", "Show usage");
+    add_option("J", value<double>(&J)->required());
+    add_option("mu", value<double>(&mu)->required());
+    add_option("shape",
+               value<std::vector<uint32_t>>(&shape)->required()->multitoken());
+    add_option("n-wolff", value<uint32_t>(&n_wolff)->required());
+    add_option("n-metropolis", value<uint32_t>(&n_metropolis)->required());
+    add_option("measure-every", value<uint32_t>(&measure_every)->required());
+    add_option("n-measure", value<uint32_t>(&n_measure)->required());
+    add_option("seed", value<uint32_t>(&seed));
+    add_option("id", value<std::string>(&id));
+    add_option("tag", value<std::string>(&tag));
+
+    variables_map vm;
+    store(parse_command_line(argc, argv, description), vm);
+    if (vm.count("help") > 0) {
+        std::cout << description << std::endl;
+        return false;
+    }
+    notify(vm);
+
+    parameters->set_j(J);
+    parameters->set_mu(mu);
+    parameters->mutable_shape()->Clear();
+    for (const uint32_t i : shape) {
+        parameters->mutable_shape()->Add(i);
+    }
+    parameters->set_n_wolff(n_wolff);
+    parameters->set_n_metropolis(n_metropolis);
+    parameters->set_measure_every(measure_every);
+    parameters->set_n_measure(n_measure);
+    parameters->set_seed(seed);
+    parameters->set_id(id);
+    parameters->set_tag(tag);
+    return true;
+}
