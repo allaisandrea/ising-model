52e5b569fa0e1e66d500385f3107c594a7dd257b
andrea <allais.andrea@gmail.com>
2019-07-22 07:38:03 -0700

    Fixed error format

--- src/compute_autocorrelation.cpp
+++ src/compute_autocorrelation.cpp
@@ -1,10 +1,13 @@
 #include "compute_autocorrelation.h"
 #include "cross_validate.h"
+#include "error_format.h"
 #include "udh_file_group.h"
 
 int main(int argc, char **argv) {
     auto pairs = ReadUdhParametersFromFiles(argv + 1, argv + argc);
     std::vector<UdhFileGroup> file_groups = GroupFiles(pairs, 3);
+    std::vector<const char *> row_headers = {"n_d0", "n_h0", "n_u0"};
+    std::vector<const char *> col_headers = {"n_d1", "n_h1", "n_u1"};
     for (auto &group : file_groups) {
         const CrossValidationStats stats = CrossValidate(
             /*n_batches=*/16,
@@ -18,6 +21,21 @@ int main(int argc, char **argv) {
                 return result;
             },
             &group);
-        std::cout << stats.mean << "\n\n" << stats.std_dev << "\n";
+        std::cout << std::setw(5) << "";
+        for (int j = 0; j < 3; ++j) {
+            std::cout << std::setw(15) << col_headers[j];
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < 3; ++i) {
+            std::cout << std::setw(5) << row_headers[i];
+            for (int j = 0; j < 3; ++j) {
+                const int k = 3 * j + i;
+                std::cout << std::setw(15)
+                          << ErrorFormat(stats.mean(k), stats.std_dev(i));
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << stats.mean << "\n\n" << stats.std_dev << "\n\n";
     }
 }
