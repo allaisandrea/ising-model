424ec4828f3a8396e990da45e23f26e0d5a01578
andrea <allais.andrea@gmail.com>
2019-10-24 09:02:40 -0700

    Account for n_read in mean times

--- src/compute_timing.h
+++ src/compute_timing.h
@@ -13,23 +13,28 @@ inline Eigen::ArrayXd ComputeTiming(uint64_t n_read, UdhFileGroup *file_group) {
     uint64_t previous_stamp = 0;
     UdhObservables observables;
     for (uint64_t i = 0; i < n_read; ++i) {
-        if (!file_group->NextObservables(&observables)) {
+        uint64_t file_index = 0;
+        if (!file_group->NextObservables(&observables, &file_index)) {
             throw std::runtime_error(
                 "Reached end of data at i = " + std::to_string(i) + " / " +
                 std::to_string(n_read));
         }
-        mean(0) += observables.flip_cluster_duration() +
-                   observables.clear_flag_duration();
-        mean(1) += observables.metropolis_sweep_duration();
-        mean(2) += observables.measure_duration();
+        const uint64_t read_every =
+            file_group->entries().at(file_index).read_every;
+        mean(0) += (observables.flip_cluster_duration() +
+                    observables.clear_flag_duration()) *
+                   read_every;
+        mean(1) += observables.metropolis_sweep_duration() * read_every;
+        mean(2) += observables.measure_duration() * read_every;
         if (observables.serialize_duration() > 0) {
-            mean(3) += observables.serialize_duration();
+            mean(3) += observables.serialize_duration() * read_every;
             mean(4) += observables.stamp() - previous_stamp -
-                       observables.flip_cluster_duration() -
-                       observables.clear_flag_duration() -
-                       observables.metropolis_sweep_duration() -
-                       observables.measure_duration() -
-                       observables.serialize_duration();
+                       (observables.flip_cluster_duration() +
+                        observables.clear_flag_duration() +
+                        observables.metropolis_sweep_duration() +
+                        observables.measure_duration() +
+                        observables.serialize_duration()) *
+                           read_every;
         }
         previous_stamp = observables.stamp();
     }
