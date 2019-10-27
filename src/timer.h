64da9183c91a24b206f39d286f4bb793213e4302
andrea <allais.andrea@gmail.com>
2019-10-27 11:16:53 -0700

    Timer: added Clock typedef

--- src/timer.h
+++ src/timer.h
@@ -2,32 +2,33 @@
 #include <chrono>
 #include <stdexcept>
 
-template <typename Clock> class Timer {
-    typename Clock::time_point _start_time;
-    typename Clock::duration _elapsed_time;
+template <typename ClockT> class Timer {
+    typename ClockT::time_point _start_time;
+    typename ClockT::duration _elapsed_time;
     enum State { STARTED, STOPPED } _state;
 
   public:
+    using Clock = ClockT;
     Timer() : _elapsed_time{}, _state(STOPPED) {}
     void start() {
         if (_state == STOPPED) {
-            _start_time = Clock::now();
+            _start_time = ClockT::now();
             _state = STARTED;
         }
     }
 
     void stop() {
         if (_state == STARTED) {
-            _elapsed_time += Clock::now() - _start_time;
+            _elapsed_time += ClockT::now() - _start_time;
             _state = STOPPED;
         }
     }
 
-    typename Clock::duration elapsed() {
+    typename ClockT::duration elapsed() {
         if (_state == STOPPED) {
             return _elapsed_time;
         } else if (_state == STARTED) {
-            return _elapsed_time + Clock::now() - _start_time;
+            return _elapsed_time + ClockT::now() - _start_time;
         } else {
             throw std::runtime_error("Corrupt state");
         }
